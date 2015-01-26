49c4350a6a85bc4f41b74e7bfb3c6aaa886b1e35
Shakil007 <mehedi_nsu07@hotmail.com>
2015-01-26 04:54:18 -0500

    delete

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Enable the asset pipeline
-config.assets.enabled = true
-config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
